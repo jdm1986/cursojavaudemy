68fd7c4c852e14e29ee950cc7a56bbb2ed927872
sportyisus <diazmartinmail@gmail.com>
2023-12-06 21:52:42 +0100

    Pasar una fecha string a Date

---
+++ src/EjemploJavaUtilDateParse.java
@@ -0,0 +1,22 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class EjemploJavaUtilDateParse {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println("Ingresa una fecha con este formato yyyy-MM-dd");
+        try {
+            Date fecha = format.parse(scanner.nextLine());
+            System.out.println("fecha = " + fecha);
+            System.out.println("format = " + format.format(fecha));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
