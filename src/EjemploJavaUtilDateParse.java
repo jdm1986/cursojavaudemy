c39910e963a85695fc7ef527871f5854820aac19
sportyisus <diazmartinmail@gmail.com>
2023-12-07 13:29:08 +0100

    Agregando una condición de salida del bucle con cadena vacía.

--- src/EjemploJavaUtilDateParse.java
+++ src/EjemploJavaUtilDateParse.java
@@ -10,7 +10,13 @@ public class EjemploJavaUtilDateParse {
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
         while (true) {
-            System.out.println("Ingresa una fecha con este formato yyyy-MM-dd");
+            System.out.println("Ingresa una fecha con este formato yyyy-MM-dd o pulse Enter para salir");
+            String input = scanner.nextLine();
+
+            if (input.equals("")){
+                break;
+            }
+
             try {
                 Date fecha = format.parse(scanner.nextLine());
                 System.out.println("fecha = " + fecha);
