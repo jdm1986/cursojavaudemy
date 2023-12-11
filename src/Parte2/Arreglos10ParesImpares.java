fceb13d64143aa08df0a79afa3986ab7a6f79f4f
sportyisus <diazmartinmail@gmail.com>
2023-12-11 17:57:29 +0100

    De un array de números, sacar dos array, uno de pares y otro de impares, usando variable de control

---
+++ src/Parte2/Arreglos10ParesImpares.java
@@ -0,0 +1,52 @@
+package Parte2;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Arreglos10ParesImpares {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] a;
+        int [] pares;
+        int [] impares;
+        int totalPares = 0;
+        int totalImpares = 0;
+
+        a = new int[10];
+
+        System.out.println("Introduce 10 números");
+        for (int i =0; i<a.length;i++){
+            a[i] = scanner.nextInt();
+        }
+
+        for (int i = 0;i <a.length;i++){
+            if (a[i] % 2 == 0){
+                totalPares++;
+
+            } else {
+                totalImpares++;
+
+            }
+        }
+        pares = new int[totalPares];
+        impares = new int[totalImpares];
+
+        int j = 0;//variable de control
+        int k = 0;//variable de control
+
+        for (int i = 0;i <a.length;i++){
+            if (a[i] % 2 == 0){
+                pares[j++] = a[i];
+
+            } else {
+                totalImpares++;
+                impares[k++] = a[i];
+            }
+        }
+
+        System.out.println("Lista de pares: "+Arrays.toString(pares));
+        System.out.println("Lista de impares: "+Arrays.toString(impares));
+    }
+}
