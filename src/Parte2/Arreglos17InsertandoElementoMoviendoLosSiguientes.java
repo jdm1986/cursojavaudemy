0c7309cf86eae049a0c7b81d9088b37bd5d1192b
sportyisus <diazmartinmail@gmail.com>
2023-12-13 12:21:25 +0100

    corregido, antes cogía la posición, y no el elemento en sí (gracias a isrita)

--- src/Parte2/Arreglos17InsertandoElementoMoviendoLosSiguientes.java
+++ src/Parte2/Arreglos17InsertandoElementoMoviendoLosSiguientes.java
@@ -7,42 +7,41 @@ public class Arreglos17InsertandoElementoMoviendoLosSiguientes {
 
     public static void main(String[] args) {
 
-        int[] arregloA = new int[10];
+        int[] arrayA = new int[10];
         Scanner scanner = new Scanner(System.in);
         int elemento;
         int posicion;
-        int ultimoElementoDelArregloA;
+        int ultimoElementoDelArrayA;
 
-        for (int i = 0;i<arregloA.length;i++){
+        for (int i = 0; i < arrayA.length; i++) {
             System.out.print("Ingresa un número: ");
-            arregloA[i] = scanner.nextInt();
+            arrayA[i] = scanner.nextInt();
         }
-        System.out.println(Arrays.toString(arregloA));
+
+
+        System.out.println(Arrays.toString(arrayA));
         System.out.println();
-        ultimoElementoDelArregloA = arregloA.length-1;
+        ultimoElementoDelArrayA = arrayA[arrayA.length - 1];
 
         System.out.println("Ingresa el valor del nuevo elemento");
         elemento = scanner.nextInt();
-        System.out.println("Ingresa la posición donde quieres insertarlo de 0-9");
-        posicion = scanner.nextInt();
+        System.out.println("Ingresa la posición donde quieres insertarlo de 1-10");
+        posicion = scanner.nextInt() - 1;
 
 
-
-        for (int i = arregloA.length-2;i >= posicion;i--){
-            arregloA[i+1] = arregloA[i];
+        for (int i = arrayA.length - 2; i >= posicion; i--) {
+            arrayA[i + 1] = arrayA[i];
         }
 
-        int [] arregloB = new int[arregloA.length+1];
+        int[] arrayB = new int[arrayA.length + 1];
 
-        System.arraycopy(arregloA,0,arregloB,0,arregloA.length);
+        System.arraycopy(arrayA, 0, arrayB, 0, arrayA.length);
 
-        arregloB[posicion] = elemento;
+        arrayB[posicion] = elemento;
 
-        arregloB[arregloB.length-1] = ultimoElementoDelArregloA;
+        arrayB[arrayB.length - 1] = ultimoElementoDelArrayA;
 
-        System.out.println(Arrays.toString(arregloB));
-
-
+        System.out.println(Arrays.toString(arrayB));
     }
 
 }
