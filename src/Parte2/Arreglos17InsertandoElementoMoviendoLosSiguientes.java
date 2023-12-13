de0a4610f588db1fd5a5d46644bfc5e1ff66969c
sportyisus <diazmartinmail@gmail.com>
2023-12-13 12:49:42 +0100

    usando un solo bucle

--- src/Parte2/Arreglos17InsertandoElementoMoviendoLosSiguientes.java
+++ src/Parte2/Arreglos17InsertandoElementoMoviendoLosSiguientes.java
@@ -1,47 +1,30 @@
 package Parte2;
 
+import javax.swing.table.AbstractTableModel;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Arreglos17InsertandoElementoMoviendoLosSiguientes {
 
     public static void main(String[] args) {
+//un solo bucle
 
-        int[] arrayA = new int[10];
-        Scanner scanner = new Scanner(System.in);
-        int elemento;
-        int posicion;
-        int ultimoElementoDelArrayA;
-
-        for (int i = 0; i < arrayA.length; i++) {
-            System.out.print("Ingresa un número: ");
-            arrayA[i] = scanner.nextInt();
-        }
-
-
-        System.out.println(Arrays.toString(arrayA));
-        System.out.println();
-        ultimoElementoDelArrayA = arrayA[arrayA.length - 1];
-
-        System.out.println("Ingresa el valor del nuevo elemento");
-        elemento = scanner.nextInt();
-        System.out.println("Ingresa la posición donde quieres insertarlo de 1-10");
-        posicion = scanner.nextInt() - 1;
-
-
-        for (int i = arrayA.length - 2; i >= posicion; i--) {
-            arrayA[i + 1] = arrayA[i];
-        }
-
+        int[] arrayA = {1, 2, 3, 4, 5};
+        int nuevoElemento = 20;
+        int posicion = 2;
         int[] arrayB = new int[arrayA.length + 1];
 
         System.arraycopy(arrayA, 0, arrayB, 0, arrayA.length);
 
-        arrayB[posicion] = elemento;
+        System.out.println(Arrays.toString(arrayB));
 
-        arrayB[arrayB.length - 1] = ultimoElementoDelArrayA;
+        for (int i = arrayA.length - 1; i >= posicion; i--) {
+            arrayB[i + 1] = arrayA[i];
+        }
+        arrayB[posicion] = nuevoElemento;
 
         System.out.println(Arrays.toString(arrayB));
+
     }
 
 }
