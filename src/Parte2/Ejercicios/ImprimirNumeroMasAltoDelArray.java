9651df7375076c6254739aefa835e53255a33829
sportyisus <diazmartinmail@gmail.com>
2023-12-13 17:15:23 +0100

    solucionando la búsqueda del valor más alto

--- src/Parte2/Ejercicios/ImprimirNumeroMasAltoDelArray.java
+++ src/Parte2/Ejercicios/ImprimirNumeroMasAltoDelArray.java
@@ -3,5 +3,15 @@ package Parte2.Ejercicios;
 public class ImprimirNumeroMasAltoDelArray {
     public static void main(String[] args) {
 
+        int[] arrayA = {14, 33, 15, 36, 78, 21, 43};
+
+        int auxiliar = 0;
+        for (int i = 0; i<arrayA.length;i++){
+            if (arrayA[i]>auxiliar){
+                auxiliar = arrayA[i];
+            }
+        }
+        System.out.println("El valor mayor es " + auxiliar);
+
     }
 }
