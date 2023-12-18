2cd384d47b15eaafc90381df96a046c4d8cb9d48
sportyisus <diazmartinmail@gmail.com>
2023-12-18 14:57:10 +0100

    Trabajando y alterando el método equals de la clase Object

--- src/Parte4ProgramacionOrientadaAObjetos/Automovil.java
+++ src/Parte4ProgramacionOrientadaAObjetos/Automovil.java
@@ -7,45 +7,77 @@ public class Automovil {
     private double cilindrada;
     private int capacidadEstanque = 40;
 
-    public String leerFabricante(){
-        return this.fabricante;
+    public Automovil() {
     }
-    public void asignarFabricante(String fabricante){
+
+    public Automovil(String fabricante, String modelo) {
         this.fabricante = fabricante;
-    }
-    public String leerModelo(){
-        return this.modelo;
-    }
-    public void asignarModelo(String modelo){
         this.modelo = modelo;
     }
-    public String leercolor(){
-        return this.color;
-    }
-    public void  asignarColor(String color){
+
+    public Automovil(String fabricante, String modelo, String color) {
+        this(fabricante, modelo);
         this.color = color;
     }
-    public Double leerCilindrada(){
-        return this.cilindrada;
-    }
-    public void asignarCilindrada(Double cilindrada){
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada) {
+        this(fabricante, modelo, color);
         this.cilindrada = cilindrada;
     }
-    public int leerCapacidadEstanque(){
-        return capacidadEstanque;
-    }
-    public void asignarCapacidadEstanque(int capacidadEstanque){
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada, int capacidadEstanque) {
+        this(fabricante, modelo, color, cilindrada);
         this.capacidadEstanque = capacidadEstanque;
     }
 
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    } //al estar en el constructor, podría prescidir del set
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCapacidadEstanque() {
+        return capacidadEstanque;
+    }
+
+    public void setCapacidadEstanque(int capacidadEstanque) {
+        this.capacidadEstanque = capacidadEstanque;
+    }
 
     public String verDetalle() {
 
         String stringBuilder =
-                /*con el this hago referencia de forma explícita al atributo de la clase y no a una variable que se llame igual dentr del contexto del método*/
-                "auto.fabricante = " + this.fabricante +
-                        "\nauto.modelo = " + this.modelo +
-                        "\nauto.color = " + this.color +
+                /*con el this hago referencia de forma explícita al atributo de la clase y no a una variable que se llame igual dentr del contexto del método
+                 * a continuación 4 formas de hacer referencia a los atributos, get, this.get, atributo mientras no haya una variable igual, o this.atributo. */
+                "auto.fabricante = " + getFabricante() +
+                        "\nauto.modelo = " + this.getModelo() +
+                        "\nauto.color = " + color +
                         "\nauto.cilindrada = " + this.cilindrada;
         /*stringBuilder.append("auto.fabricante = " + this.fabricante);
         stringBuilder.append("\nauto.modelo = " + this.modelo);
@@ -74,6 +106,17 @@ public class Automovil {
     }
 
     public float calcularConsumo(int km, int porcentajeCombustible) {
-        return km / (this.capacidadEstanque * (porcentajeCombustible/100f));
+        return km / (this.capacidadEstanque * (porcentajeCombustible / 100f));
     }
-}
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (!(obj instanceof Automovil)) {
+            return false;}
+            Automovil a = (Automovil) obj;
+            return (this.fabricante != null && this.modelo != null && this.fabricante.equals(a.getFabricante()) && this.modelo.equals(a.getModelo()));
+        }
+    }
--- src/Parte4ProgramacionOrientadaAObjetos/Poo01Automovil.java
+++ src/Parte4ProgramacionOrientadaAObjetos/Poo01Automovil.java
@@ -1,24 +1,32 @@
 package Parte4ProgramacionOrientadaAObjetos;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Date;
+
 public class Poo01Automovil {
     public static void main(String[] args) {
 
-    Automovil subaru = new Automovil();
+    Automovil subaru = new Automovil("subauru","Impreza");
+    //al implementar el constructor con parámetros de la clase, no necesito especialmente el set. Ya que estamos creando e inicializando
+        // el objeto con el fabricante y el modelo.
 
-        subaru.asignarFabricante("subauru");
-        subaru.asignarModelo("Impreza");
-        subaru.asignarCilindrada(2.0);
-        subaru.asignarColor("blanco");
+        subaru.setCilindrada(2.0);
+        subaru.setColor("blanco");
 
-        Automovil mazda = new Automovil();
-
-        mazda.asignarFabricante("Mazda");
-        mazda.asignarModelo("Bt-50");
-        mazda.asignarCilindrada(3.0);
-        mazda.asignarColor("rojo");
-
-        System.out.println("mazda.leerFabricante() = " + mazda.leerFabricante());
+        Automovil mazda = new Automovil("Mazda","Bt-50","Rojo",3.0);
+        System.out.println("mazda.leerFabricante() = " + mazda.getFabricante());
         System.out.println();
+
+        Automovil nissan = new Automovil("Nissan","Navara","Gris Oscuro",3.5,50);
+        Automovil nissan2 = new Automovil("Nissan","Navara","Gris Oscuro",3.5,50);
+
+        Automovil auto =new Automovil(  );
+        Date fecha = new Date();
+
+        System.out.println("Son iguales nissan y nissan2? " + (nissan == nissan2));
+        System.out.println("Son iguales nissan y nissan2 con equals? " + nissan.equals(nissan2)); // se ha modificado la clase padre = principio de polimorfismo
+        System.out.println();
+
 
 
         /*System.out.println("mazda.fabricante = " + mazda.fabricante);
@@ -30,9 +38,15 @@ public class Poo01Automovil {
         System.out.println();
         System.out.println(mazda.verDetalle());
         System.out.println();
+        System.out.println(nissan.verDetalle());
+        System.out.println();
+        System.out.println(auto.equals(fecha));
+        System.out.println();
         System.out.println(subaru.acelerar(3000));
         System.out.println(subaru.frenar());
         System.out.println();
+
+
 
         System.out.println(mazda.acelerarYFrenar(4000));
         System.out.println();
@@ -40,6 +54,7 @@ public class Poo01Automovil {
         System.out.println("Kilómetros por litro " + subaru.calcularConsumo(300,0.6f));
         //mismo método diferente implementación (principio de encapsulamiento y polimorfismo) se suele confundir con con el principio de ocultación o visibildiad (público.. privado.. protegido)...
         System.out.println("Kilómetros por litro " + subaru.calcularConsumo(300,60));
+        System.out.println("Kilómetros por litro " + nissan.calcularConsumo(300,60));
 
 
 
