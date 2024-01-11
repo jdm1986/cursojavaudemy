e598c2083774d56721b1faa184d92b80633f6fad
sportyisus <diazmartinmail@gmail.com>
2024-01-11 18:15:45 +0100

    Complicado esta parte con las clases abstractas.

--- src/com/jdiaz/parte9curso_poo_clases_abstractas/form/EjemploForm.java
+++ src/com/jdiaz/parte9curso_poo_clases_abstractas/form/EjemploForm.java
@@ -5,6 +5,7 @@ import com.jdiaz.parte9curso_poo_clases_abstractas.form.elementos.InputForm;
 import com.jdiaz.parte9curso_poo_clases_abstractas.form.elementos.SelectForm;
 import com.jdiaz.parte9curso_poo_clases_abstractas.form.elementos.TextareaForm;
 import com.jdiaz.parte9curso_poo_clases_abstractas.form.elementos.select.Opcion;
+import com.jdiaz.parte9curso_poo_clases_abstractas.form.validador.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,19 +14,26 @@ public class EjemploForm {
     public static void main(String[] args) {
 
         InputForm ursername = new InputForm("username"); //tipo text por defecto
+        ursername.addValidador(new RequeridoValidador());
         InputForm password = new InputForm("clave", "password");
+        password.addValidador(new RequeridoValidador());
+        password.addValidador(new LargoValidador(6, 12));
         InputForm email = new InputForm("email", "email");
+        email.addValidador(new RequeridoValidador());
+        email.addValidador(new EmailValidador());
         InputForm edad = new InputForm("edad", "numbre");
+        edad.addValidador(new NumeroValidador());
 
         TextareaForm experiencia = new TextareaForm("exp", 5, 9);
 
         SelectForm lenguaje = new SelectForm("lenguaje");
+        lenguaje.addValidador(new NoNuloValidador());
         Opcion java = new Opcion("1", "Java");
         lenguaje.addOpcion(java);
-        lenguaje.addOpcion(new Opcion("2","Python"));
+        lenguaje.addOpcion(new Opcion("2", "Python"));
         lenguaje.addOpcion(new Opcion("3", "JavaScript"));
         lenguaje.addOpcion(new Opcion("4", "TypeScript"));
-        lenguaje.addOpcion(new Opcion("5","PHP"));
+        lenguaje.addOpcion(new Opcion("5", "PHP"));
 
 
         ElementoForm saludar = new ElementoForm() {
@@ -37,14 +45,14 @@ public class EjemploForm {
 
 
         saludar.setValor("Hola que tal este campo está deshabilitado");
-        ursername.setValor("John.doe");
-        password.setValor("a1b2c3");
-        email.setValor("john.doe@correo.com");
+        ursername.setValor("");
+        password.setValor("a1b2c");
+        email.setValor("john.doecorreo.com");
         edad.setValor("29");
         experiencia.setValor(".... mas de 10 años de experiencia....");
         java.setSelected(true);
 
-        List <ElementoForm> elementos = new ArrayList<>();
+        List<ElementoForm> elementos = new ArrayList<>();
         elementos.add(ursername);
         elementos.add(password);
         elementos.add(email);
@@ -59,5 +67,10 @@ public class EjemploForm {
             System.out.println("<br>");
         }
 
+        for (ElementoForm e : elementos) {
+            if (!e.esValido()) {
+                e.getErrores().forEach(err -> System.out.println(err));
+            }
+        }
     }
 }
