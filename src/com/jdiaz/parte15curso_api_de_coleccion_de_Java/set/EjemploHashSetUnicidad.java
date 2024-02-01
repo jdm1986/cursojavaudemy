ca5695098460a4dc660a9edf93d0b56cf4909278
sportyisus <diazmartinmail@gmail.com>
2024-02-01 15:08:30 +0100

    singularidad de treeset y hashset

--- src/com/jdiaz/parte15curso_api_de_coleccion_de_Java/set/EjemploHashSetUnicidad.java
+++ src/com/jdiaz/parte15curso_api_de_coleccion_de_Java/set/EjemploHashSetUnicidad.java
@@ -2,22 +2,24 @@ package com.jdiaz.parte15curso_api_de_coleccion_de_Java.set;
 
 import com.jdiaz.parte15curso_api_de_coleccion_de_Java.modelo.Alumno;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class EjemploTreeSetComparable {
+public class EjemploHashSetUnicidad {
     public static void main(String[] args) {
 
-        Set<Alumno> alumnoSet = new TreeSet<>((a,b)-> b.getNota().compareTo(a.getNota()));
+        Set<Alumno> alumnoSet = new HashSet<>();
 
         alumnoSet.add(new Alumno("Paco",5));
         alumnoSet.add(new Alumno("Bea",7));
         alumnoSet.add(new Alumno("Luis",9));
         alumnoSet.add(new Alumno("Jano",3));
-        alumnoSet.add(new Alumno("Zeus",4));
-        alumnoSet.add(new Alumno("Zeus",4));
         alumnoSet.add(new Alumno("Juan",10));
         alumnoSet.add(new Alumno("Lorena",6));
+        alumnoSet.add(new Alumno("Zeus",2));
+        alumnoSet.add(new Alumno("Zeus",3));
+        alumnoSet.add(new Alumno("Lucas",2));
 
         System.out.println("alumnoSet = " + alumnoSet);
 
