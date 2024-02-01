a7fe730b4699649f83259c42720bcf870f3c0067
sportyisus <diazmartinmail@gmail.com>
2024-02-01 21:27:10 +0100

    formas de loop o iteraciones en los collection set y list

--- src/com/jdiaz/parte15curso_api_de_coleccion_de_Java/set/EjemploHashSetUnicidad.java
+++ src/com/jdiaz/parte15curso_api_de_coleccion_de_Java/set/EjemploHashSetUnicidad.java
@@ -2,14 +2,14 @@ package com.jdiaz.parte15curso_api_de_coleccion_de_Java.set;
 
 import com.jdiaz.parte15curso_api_de_coleccion_de_Java.modelo.Alumno;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class EjemploHashSetUnicidad {
     public static void main(String[] args) {
 
         Set<Alumno> alumnoSet = new HashSet<>();
+
+        //List<Alumno> alumnoSet = new ArrayList<>();
 
         alumnoSet.add(new Alumno("Paco",5));
         alumnoSet.add(new Alumno("Bea",7));
@@ -20,8 +20,37 @@ public class EjemploHashSetUnicidad {
         alumnoSet.add(new Alumno("Zeus",2));
         alumnoSet.add(new Alumno("Zeus",3));
         alumnoSet.add(new Alumno("Lucas",2));
+        alumnoSet.add(new Alumno("Laura",10));
 
         System.out.println("alumnoSet = " + alumnoSet);
+
+        System.out.println("Utilizando un for clásico");
+
+        /*for (int i = 0; i < alumnoSet.size();i++){
+            Alumno a = alumnoSet.get(i);
+            System.out.println(a.getNombre());
+        }*/
+
+        System.out.println("Iterando usando un foreach");
+
+        /*for (int i = 0; i<alumnoSet.toArray().length;i++){
+            System.out.println(alumnoSet.toArray()[i]);
+        }*/
+        for (Alumno alumnos : alumnoSet) {
+            System.out.println(alumnos);
+        }
+
+        System.out.println("Iterando usando while e iterator");
+
+        Iterator<Alumno> iterator = alumnoSet.iterator();
+        while (iterator.hasNext()){
+            Alumno a = iterator.next();
+            System.out.println(a.getNombre());
+        }
+
+        System.out.println("Iterando con expresión lambda");
+
+        alumnoSet.forEach(System.out::println);
 
     }
 }
