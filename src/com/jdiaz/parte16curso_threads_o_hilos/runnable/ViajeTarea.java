516260fd23cb6f372ab55301275d3bd2b7648dec
sportyisus <diazmartinmail@gmail.com>
2024-02-04 20:09:58 +0100

    creando hilos implementando la interface Runnable

---
+++ src/com/jdiaz/parte16curso_threads_o_hilos/runnable/ViajeTarea.java
@@ -0,0 +1,28 @@
+package com.jdiaz.parte16curso_threads_o_hilos.runnable;
+
+public class ViajeTarea implements Runnable{
+
+    private String nombre;
+
+    public ViajeTarea(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0;i <10;i++){
+            System.out.println(i + " - " + nombre);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Finalmente me voy de viaje a: " +nombre);
+    }
+}
