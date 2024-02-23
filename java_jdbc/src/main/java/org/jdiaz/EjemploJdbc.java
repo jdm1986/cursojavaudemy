d564bd7324811aff3c002bea570eb6812cae5e04
sportyisus <diazmartinmail@gmail.com>
2024-02-23 11:56:22 +0100

    Creando Proyecto Java con JDBC

---
+++ java_jdbc/src/main/java/org/jdiaz/EjemploJdbc.java
@@ -0,0 +1,28 @@
+package org.jdiaz;
+
+import java.sql.*;
+
+public class EjemploJdbc {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/java_curso";
+        String username = "root";
+        String password = "$1482Zaid$";
+        try {
+            Connection connection = DriverManager.getConnection(url,username,password);
+            Statement statement = connection.createStatement();
+            ResultSet resultado = statement.executeQuery("SELECT * FROM productos");
+
+            while (resultado.next()){
+                System.out.println(resultado.getString("nombre"));
+            }
+            resultado.close();
+            statement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ src/com/jdiaz/parte28curso_BBDD_con_JDBC/Prueba.java
@@ -0,0 +1,14 @@
+package com.jdiaz.parte28curso_BBDD_con_JDBC;
+
+public class Prueba {
+    public static void main(String[] args) {
+
+        String nombre = "Nombre";
+        System.out.println("nombre = " + nombre);
+
+
+
+    }
+
+
+}
