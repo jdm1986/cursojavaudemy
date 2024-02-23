2da2bd9a2ba63fa235ca68d50a38dfa391b29a82
sportyisus <diazmartinmail@gmail.com>
2024-02-23 13:25:13 +0100

    cerrando conexxiones, recursos y manejo de errores

--- java_jdbc/src/main/java/org/jdiaz/EjemploJdbc.java
+++ java_jdbc/src/main/java/org/jdiaz/EjemploJdbc.java
@@ -8,20 +8,24 @@ public class EjemploJdbc {
         String url = "jdbc:mysql://localhost:3306/java_curso";
         String username = "root";
         String password = "$1482Zaid$";
-        try {
-            Connection connection = DriverManager.getConnection(url,username,password);
-            Statement statement = connection.createStatement();
-            ResultSet resultado = statement.executeQuery("SELECT * FROM productos");
+
+        try ( Connection connection = DriverManager.getConnection(url,username,password); //de esta forma se hace autoClose
+        Statement statement = connection.createStatement();
+        ResultSet resultado = statement.executeQuery("SELECT * FROM productos")) {
 
             while (resultado.next()){
-                System.out.println(resultado.getString("precio"));
+                System.out.print(resultado.getString("nombre"));
+                System.out.print(" | ");
+                System.out.print(resultado.getInt("id"));
+                System.out.print(" | ");
+                System.out.print(resultado.getInt("precio"));
+                System.out.print(" | ");
+                System.out.println(resultado.getDate("fecha_registro"));
             }
-            resultado.close();
-            statement.close();
-            connection.close();
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+
         }
 
     }
