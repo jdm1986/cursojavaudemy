7915d4f5255e230812cd16f4d92dbb7ce01bac35
sportyisus <diazmartinmail@gmail.com>
2024-03-15 14:24:02 +0100

    Empezando Proyecto

--- src/main/java/org/jdiaz/junit5app/ejemplos/models/Cuenta.java
+++ src/main/java/org/jdiaz/junit5app/ejemplos/models/Cuenta.java
@@ -9,6 +9,19 @@ public class Cuenta {
     private String persona;
     private BigDecimal saldo;
 
+    public String getPersona() {
+        return persona;
+    }
 
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
 
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
 }
