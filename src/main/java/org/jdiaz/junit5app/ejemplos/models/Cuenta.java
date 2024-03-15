6f20a9a8199fabbba6063517a9d3a70ceeec6444
sportyisus <diazmartinmail@gmail.com>
2024-03-15 14:01:27 +0100

    Empezando Proyecto

---
+++ src/main/java/org/jdiaz/junit5app/ejemplos/models/Cuenta.java
@@ -0,0 +1,14 @@
+package org.jdiaz.junit5app.ejemplos.models;
+
+import org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.BigDecimalConversion;
+
+import java.math.BigDecimal;
+
+public class Cuenta {
+
+    private String persona;
+    private BigDecimal saldo;
+
+
+
+}
